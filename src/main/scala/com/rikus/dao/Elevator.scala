84436bf75c5abe2ca942b0feebd1960e6023674d
Rikus de Milander <rikus@mn8.ee>
2020-05-07 17:48:41 +0200

    working, just need to finishe smart next destination logic with the optional direction.

--- src/main/scala/com/rikus/dao/Elevator.scala
+++ src/main/scala/com/rikus/dao/Elevator.scala
@@ -7,13 +7,12 @@ import scala.collection.mutable.ListBuffer
 
 case class Elevator(id: Int, initialState: (0, 0)) extends Actor with ActorLogging {
   var currentStatus: elevatorStatus = elevatorStatus(initialState._1, initialState._2)
-  val destinations: scala.collection.mutable.ListBuffer[(Int,Option[Direction])] = ListBuffer.empty[(Int, Option[Direction])] //todo : Fix to incorporate direction into destinations
+  val destinations: scala.collection.mutable.ListBuffer[(Int, Option[Direction])] = ListBuffer.empty[(Int, Option[Direction])]
 
   def nextDestination = {
     destinations.headOption match {
-      case Some((floorDest,direction)) =>
-        // todo : Fix the next destination smart logic 
-        destinations -= floorDest
+      case Some((floorDest, direction)) => //always go to the oldest destination first
+        destinations -= ((floorDest, direction))
         floorDest
       case None =>
         log.info("No known next destination")
@@ -21,13 +20,9 @@ case class Elevator(id: Int, initialState: (0, 0)) extends Actor with ActorLoggi
     }
   }
 
-  def addDestination(floor: Int, direction:Option[Direction]) = {
-
-    if (!(destinations.contains((floor,None)) || destinations.contains((floor,Some(_)))))
-      destinations += ((floor,direction))
-
-//    if (!destinations.contains(floor))
-//      destinations += floor
+  def addDestination(floor: Int, direction: Option[Direction]) = {
+    if (!(destinations.contains((floor, None)) || destinations.contains((floor, Some(_: Direction)))))
+      destinations += ((floor, direction))
   }
 
   def receive: Receive = {
@@ -35,11 +30,11 @@ case class Elevator(id: Int, initialState: (0, 0)) extends Actor with ActorLoggi
       log.info(s"Elevator ${this.id} sending status ...")
       sender() ! currentStatus
     case pickup(floor, direction) =>
-      addDestination(floor,Some(direction)) //add destination with direction
+      addDestination(floor, Some(direction)) //add destination with direction
       log.info(s"Destinations for ${self} now include : ${destinations.toList}")
     case stepRequest: step => currentStatus = currentStatus.copy(currentStatus.destinationFloor, nextDestination) //change current status to next destination and remove that destination from pool
     case dropOff(floor) =>
-      addDestination(floor,None)
+      addDestination(floor, None)
       log.info(s"Destinations for ${self} now include : ${destinations.toList}")
   }
 }
