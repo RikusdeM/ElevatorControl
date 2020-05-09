dd4f082984a0314c88e0c40d643bed28bff09403
Rikus de Milander <rikus@mn8.ee>
2020-05-09 20:29:55 +0200

    busy with readme and removed old json formatters.

---
+++ ReadMe.md
@@ -0,0 +1,16 @@
+# Elevator Control System [Scala Coding Challenge]
+
+1. [Solution](#Solution)
+2. [Data Structures](#DataStructures)
+3. [Build](#Build)
+4. [Run](#Run)
+
+
+## Solution
+
+## DataStructures
+
+## Build
+This project utilizes sgs df
+
+## [Fourth Example](http://www.fourthexample.com) 
--- src/main/scala/com/rikus/JsonFormats.scala
+++ src/main/scala/com/rikus/JsonFormats.scala
@@ -7,11 +7,6 @@ import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 object JsonFormats  {
   // import the default encoders for primitive types (Int, String, Lists etc)
   import DefaultJsonProtocol._
-
-  implicit val ElevatorControlFormat: RootJsonFormat[ElevatorControl] = jsonFormat6(ElevatorControl.apply)
-  implicit val ElevatorControlDecodedFormat: RootJsonFormat[ElevatorControlDecoded] = jsonFormat6(ElevatorControlDecoded.apply)
-
   implicit val elevatorStatusFormat:RootJsonFormat[elevatorStatus] = jsonFormat2(elevatorStatus)
-
 }
 //#json-formats
